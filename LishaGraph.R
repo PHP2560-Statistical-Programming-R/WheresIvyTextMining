cb048308abb63105affa7e98e4bd93d5a8f4ed1b
wls009 <lishaw1995@gmail.com>
2017-11-07 18:44:02 -0500

    clean up

--- LishaGraph.R
+++ LishaGraph.R
@@ -11,16 +11,16 @@ tweets<-read.csv("AllFormattedTweets.csv")
 tweets$date<-as.Date(tweets$date.stamp)
 tweets$yearmon<-format(tweets$date,"%Y-%m")
 
-tweets <- tweets %>%
+retweets <- tweets %>%
   group_by(yearmon) %>%
   filter(retweetCount==max(retweetCount)) %>%
   select(text, screenName,yearmon,retweetCount)
-
+save(retweets,file="data/retweets.Rda")
 # Tokenize text into words and remove stop words
-tweets$text<-as.character(tweets$text)
-tweets <-tweets %>%
+retweets$text<-as.character(retweets$text)
+retweets <-retweets %>%
   unnest_tokens(word,text) 
-clean_tweets <- anti_join(tweets,stop_words,by="word")
+clean_tweets <- anti_join(retweets,stop_words,by="word")
 clean_tweets <- clean_tweets %>%
   filter(word != "de")
 top_tweets <-clean_tweets %>%
@@ -31,6 +31,10 @@ top_tweets <-clean_tweets %>%
   arrange(desc(n))
 
 #ggplot
+dir.create("graph/",showWarnings = F)
+png('graph/plot.png')
+
 ggplot(top_tweets) +
   geom_bar(mapping = aes(x=reorder(word,n),y=n),stat="identity")+xlab("words") +ylab("Frequency") + coord_flip()+
   ggtitle("Common words among tweets with highest retweets monthly") + theme(axis.text.x = element_text(angle = 0, hjust = 0.5, vjust = 0.5),                plot.title = element_text(size = 11,face="bold"))
+dev.off()
