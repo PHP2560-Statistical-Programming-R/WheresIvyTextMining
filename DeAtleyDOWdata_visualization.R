75c91fc0d55589648f23053ff7ef83b1a355d574
tedeatley <teresa_deatley@brown.edu>
2017-11-08 14:11:08 -0500

    Updates 11.8

--- DeAtleyDOWdata_visualization.R
+++ DeAtleyDOWdata_visualization.R
@@ -1,3 +1,6 @@
+
+#Packages
+
 library(twitteR)
 library(ROAuth)
 library(tidyverse)
@@ -10,6 +13,7 @@ library(stm)
 library(geometry)
 library(rsvd)
 library(Rtsne)
+library(ggplot2)
 
 clean.beer <- read_csv("AllFormattedTweets.csv")
 
@@ -225,25 +229,41 @@ DOW.GUS.SAT <-
 
 DOW.GUS.SAT
 
-
-
 #Barpolts of the top 5 most frequently tweeted topics on Saturdays by Company Name
+#ggplot
+
+dir.create("graph/",showWarnings = F)
+
 
 v = data.frame(topic=c('22','35','14','26','5'),n=c(89,69,50,26,24))
 barplot(v$n, names.arg=y$topic, main = "Top 5 Tweeted topics on Saturdays by Blue Moon Beer Co.", ylim = c(0,100),ylab = "Count", xlab = "Topic", col=c("burlywood1","coral"))
+ggsave('graph/Blue Moon_Top5_Topics_Sat.png')
+
+
+
 
 w = data.frame(topic=c('37','28','36','31','2'),n=c(54,16,11,8,5))
 barplot(w$n, names.arg=w$topic, main = "Top 5 Tweeted topics on Saturdays by Dos Equis Beer Co.", ylim = c(0,60),ylab = "Count", xlab = "Topic", col=c("burlywood1","coral"))
+
+ggsave('graph/Dos Equis Top 5 Topics on Sat.png')
+
 
 
 x = data.frame(topic=c('33','27','10','37','32'),n=c(111,37,31,22,19))
 barplot(x$n, names.arg=x$topic, main = "Top 5 Tweeted topics on Saturdays by Budlight Beer Co.", ylim = c(0,120),ylab = "Count", xlab = "Topic",col=c("burlywood1","coral"))
 
+ggsave('graph/Budlight Top 5 Topics on Sat.png')
+
+
 
 y = data.frame(topic=c('20','19','38','3', '16'),n=c(8,3,3,1,1))
 barplot(y$n, names.arg=y$topic, main = "Top 5 Tweeted topics on Saturdays by Tsingtao Beer Co.", ylim = c(0,10),ylab = "Count", xlab = "Topic",col=c("burlywood1","coral") )
+
+ggsave('graph/Tsingtao Top 5 Topics on Sat.png')
+
 
 
 z = data.frame(topic=c('11','5','25','29','34'),n=c(114,7,3,3,3))
 barplot(z$n, names.arg=z$topic, main = "Top 5 Tweeted topics on Saturdays by Guiness US Beer Co.", ylim = c(0,120),ylab = "Count", xlab = "Topic", col=c("burlywood1","coral"))
 
+ggsave('graph/Guiness US Top 5 Topics on Sat.png')
--- README.md
+++ README.md
@@ -6,7 +6,7 @@ TeamWheresIvyNewRepository
 #We choose 5 brands of beer from different multinational companies around the world for our analysis.Budlight, Dos Equis, Guinness U.S., Blue Moon and Tsingtao. Each of us cleaned the data for our assigned beer companies and we merged our clean data. Our data wrangleing file was sent to you separately due to twitter authorization code privacy.
 
 #Questions
-* 1.Derek, Sentiment Analysis by day of the week, Does sentiment analysis change of the     course of the week by each beer company?
+*1.Derek, Sentiment Analysis by day of the week, Does sentiment analysis change of the     course of the week by each beer company?
 *2.Michael, Thematic Topic Modeling, What are the most popular topics overall? 
 *3.Tess, What are the most popular topics on Saturdays across the 5 companies?
 *4 Lisha, Among the highest retweets each month, which are the most popular words?
